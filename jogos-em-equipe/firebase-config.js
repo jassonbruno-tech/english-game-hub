3ead46abc773d92322f554e1c4195254ed4a4e16
jassonbruno-tech <jassonbruno@gmail.com>
2026-09-17 15:47:24 -0300

    Adiciona arquivo de configuracao Firebase do multiplayer

---
+++ jogos-em-equipe/firebase-config.js
@@ -0,0 +1,11 @@
+// Configuração do Firebase Realtime Database para o multiplayer do Tug of War.
+// Cole aqui os dados do seu app Web do Firebase.
+window.TUG_FIREBASE_CONFIG = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
